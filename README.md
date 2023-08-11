fdbaa0839f65ca5af401c4abb4cebffe76c69d04
Nick Addison <nick@addisonbrown.com.au>
2023-08-11 15:15:50 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# v4-template
+### **A template for writing Uniswap v4 Hooks 🦄**
+
+[`Use this Template`](https://github.com/saucepoint/v4-template/generate)
+
+1. The example hook [Counter.sol](src/Counter.sol) demonstrates the `beforeSwap()` and `afterSwap()` hooks
+2. The test template [Counter.t.sol](test/Counter.t.sol) preconfigures the v4 pool manager, test tokens, and test liquidity.
+
+---
+
+### Local Development (Anvil)
+
+*requires [foundry](https://book.getfoundry.sh)*
+
+```
+forge install
+forge test
+```
+
+Because v4 exceeds the bytecode limit of Ethereum and it's *business licensed*, we can only deploy & test hooks on [anvil](https://book.getfoundry.sh/anvil/).
+
+```bash
+# start anvil, with a larger code limit
+anvil --code-size-limit 30000
+
+# in a new terminal
+forge script script/Counter.s.sol \
+    --rpc-url http://localhost:8545 \
+    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 \
+    --code-size-limit 30000 \
+    --broadcast
+```
+
+---
+
+Additional resources:
+
+[v4-periphery](https://github.com/uniswap/v4-periphery) contains advanced hook implementations that serve as a great reference
+
+[v4-core](https://github.com/uniswap/v4-core)
+
---
+++ src/Counter.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/libraries/PoolId.sol";
+import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+
+contract Counter is BaseHook {
+    using PoolIdLibrary for IPoolManager.PoolKey;
+
+    uint256 public beforeSwapCount;
+    uint256 public afterSwapCount;
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return Hooks.Calls({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeModifyPosition: false,
+            afterModifyPosition: false,
+            beforeSwap: true,
+            afterSwap: true,
+            beforeDonate: false,
+            afterDonate: false
+        });
+    }
+
+    function beforeSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata)
+        external
+        override
+        returns (bytes4)
+    {
+        beforeSwapCount++;
+        return BaseHook.beforeSwap.selector;
+    }
+
+    function afterSwap(address, IPoolManager.PoolKey calldata, IPoolManager.SwapParams calldata, BalanceDelta)
+        external
+        override
+        returns (bytes4)
+    {
+        afterSwapCount++;
+        return BaseHook.afterSwap.selector;
+    }
+}
